487eef6f82c321083790c2115235c98cdc08722d
unknown <linabadr48@gmail.com>
2023-05-09 15:38:03 +0300

    adding validation

--- category/serializers.py
+++ category/serializers.py
@@ -12,31 +12,34 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = ['id' ,'name']
       
-    def validate_name(self, value):
-        pattern = r'[A-Za-z]'
-        if not re.match(pattern, value):
-            raise serializers.ValidationError('Name must be string')
+    # def validate_name(self, value):
+    #     pattern = r'[A-Za-z]'
+    #     if not re.match(pattern, value):
+    #         raise serializers.ValidationError('Name must be string')
     
         
-        if len(value) < 4:
-            raise serializers.ValidationError({"Name length must be greater than 3  characters."})
+    #     if len(value) < 4:
+    #         raise serializers.ValidationError({"Name length must be greater than 3  characters."})
         
-        return value
+    #     if len(value) > 20:
+    #         raise serializers.ValidationError({"Name length must be less than 20  characters."})
+        
+    #     return value
 
     
-    def create(self, validated_data):
-        if Category.objects.filter(name=self.validated_data['name']).exists():
-            raise serializers.ValidationError({'error': 'Category name already exists!'})
+    # def create(self, validated_data):
+    #     if Category.objects.filter(name=self.validated_data['name']).exists():
+    #         raise serializers.ValidationError({'error': 'Category name already exists!'})
       
-        category = Category.objects.create(**validated_data)
-        return category
+    #     category = Category.objects.create(**validated_data)
+    #     return category
 
-    def update(self, instance, validated_data):
-        if Category.objects.filter(name=self.validated_data['name']).exists():
-            raise serializers.ValidationError({'error': 'Category name already exists!'})
-        instance.name = validated_data.get('name', instance.name)
-        instance.save()
-        return instance
+    # def update(self, instance, validated_data):
+    #     if Category.objects.filter(name=self.validated_data['name']).exists():
+    #         raise serializers.ValidationError({'error': 'Category name already exists!'})
+    #     instance.name = validated_data.get('name', instance.name)
+    #     instance.save()
+    #     return instance
 
 
 
--- product/views.py
+++ product/views.py
@@ -9,7 +9,7 @@ from rest_framework.response import Response
 from rest_framework import status, permissions
 from rest_framework.pagination import PageNumberPagination
 
-@api_view(['GET', 'POST'])
+@api_view(['GET'])
 
 def product_list (request):
     if request.method == 'GET':
@@ -20,16 +20,16 @@ def product_list (request):
         serializer = ProductSerializer(result_page, many=True)
         return paginator.get_paginated_response(serializer.data)    
 
-    if request.method == 'POST':
-      permission_classes = [permissions.IsAdminUser]
-      serializer = ProductSerializer(data=request.data)
-      if serializer.is_valid():
-          serializer.save ()
-          return Response (serializer.data, status=status.HTTP_201_CREATED)
-      else:
-          return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # if request.method == 'POST':
+    #   permission_classes = [permissions.IsAdminUser]
+    #   serializer = ProductSerializer(data=request.data)
+    #   if serializer.is_valid():
+    #       serializer.save ()
+    #       return Response (serializer.data, status=status.HTTP_201_CREATED)
+    #   else:
+    #       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
       
-@api_view(['GET', 'PUT','DELETE'])
+@api_view(['GET'])
 def product_details (request,id):
 
     try:
@@ -41,15 +41,15 @@ def product_details (request,id):
       serializer =  ProductSerializer(product)
       return Response(serializer.data)
     
-    elif request.method == 'PUT':
-      permission_classes = [permissions.IsAdminUser]
-      serializer =  ProductSerializer (product, data=request.data,partial=True)
-      if serializer.is_valid():
-         serializer.update (product, serializer.validated_data)
-         return Response (serializer.data)
-      return Response (serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # elif request.method == 'PUT':
+    #   permission_classes = [permissions.IsAdminUser]
+    #   serializer =  ProductSerializer (product, data=request.data,partial=True)
+    #   if serializer.is_valid():
+    #      serializer.update (product, serializer.validated_data)
+    #      return Response (serializer.data)
+    #   return Response (serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    elif request.method == 'DELETE' :
-      permission_classes = [permissions.IsAdminUser]
-      product.delete()
-      return Response (status=status. HTTP_204_NO_CONTENT)
+    # elif request.method == 'DELETE' :
+    #   permission_classes = [permissions.IsAdminUser]
+    #   product.delete()
+    #   return Response (status=status. HTTP_204_NO_CONTENT)
