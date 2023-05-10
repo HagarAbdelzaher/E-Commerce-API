a540d27ffa8a9411b21155b01ecbaca5173aa7ad
HagarAbdelzaher <hagarr.abdelzaher@gmail.com>
2023-05-10 04:57:09 -0700

    added address

--- user/views.py
+++ user/views.py
@@ -5,33 +5,39 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
-from .serializers import SignUpSerializer
+from .serializers import SignUpSerializer , AddressSerializer
+from.models import Address
 @api_view(['POST'])
 def signupView(request):
     if request.method == 'POST':
+        #send request data to serializer
         serializer = SignUpSerializer(data=request.data)
+        address_serializer = AddressSerializer(data=request.data.get('address'));
         
         data = {}
-        
+        #check validity of requests of address and user
         if serializer.is_valid():
-            account = serializer.save()
+            if address_serializer.is_valid():
+                account = serializer.save()
+                address_serializer.save(user=account)
+            else:
+                return Response(address_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
             
+            # sending response back
             data['response'] = "Registration Successful!"
             data['username'] = account.username
             data['email'] = account.email
             data['first_name'] = account.first_name
             data['last_name'] = account.last_name
-        
-            # data['profile_pic'] = account.profile_pic
+            data['address'] = address_serializer.data
+            
             token = Token.objects.get(user=account).key
             data['token'] = token
-            print(data)
-       
+    
         else:
             data = serializer.errors
         
         return Response(data, status=status.HTTP_201_CREATED)
-    
 
 @api_view(['POST'])
 def logoutView(request):
