9776e085a6a80da43134b69e41f10d375397d23a
HagarAbdelzaher <hagarr.abdelzaher@gmail.com>
2023-05-16 05:20:17 +0300

    added cors

--- api/settings.py
+++ api/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "category",
     "shopping_cart",
     "wishlist",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -56,6 +57,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware"
 ]
 
 ROOT_URLCONF = "api.urls"
